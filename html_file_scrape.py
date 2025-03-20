27fb01f09d0c6cd1127e99e965c9b57655f67460
jovannaflor <jovannaflor@gmail.com>
2025-03-21 00:50:55 +0200

    added example

--- html_file_scrape.py
+++ html_file_scrape.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
 
-filename = "html/bme_notices.html"
+filename = "html/filename_example.html"
 with open(filename, "r", encoding="utf-8") as file:
     html = file.read()
